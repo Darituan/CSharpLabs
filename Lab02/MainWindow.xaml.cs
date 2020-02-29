c84e111f512e7af1520aaccaffe155161dfe0d55
Dan Darituan <darituan8@gmail.com>
2020-02-29 18:41:30 +0200

    update App xaml and code behind and MainWindow xaml and code behind

---
+++ Lab02/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using Lab02.ViewModels;
+
+namespace Lab02
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainWindowViewModel();
+        }
+    }
+}
