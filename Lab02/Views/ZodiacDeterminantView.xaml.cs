d4c15da90393d85d5822e5c2dd9af933443dc1a5
Dan Darituan <darituan8@gmail.com>
2020-02-29 18:39:40 +0200

    add Views

---
+++ Lab02/Views/ZodiacDeterminantView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+using Lab02.ViewModels.ZodiacDeterminant;
+
+namespace Lab02.Views
+{
+    public partial class ZodiacDeterminantView : UserControl
+    {
+        public ZodiacDeterminantView()
+        {
+            InitializeComponent();
+            DataContext = new ZodiacDeterminantViewModel();
+        }
+    }
+}
