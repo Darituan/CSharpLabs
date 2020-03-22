d9c70b85c1448b333d9c1096e6b2c6684c234aa7
Darituan <darituan8@gmail.com>
2020-03-22 12:52:37 +0200

    add navigation manager and main window viewmodel

---
+++ Lab04/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System.Windows;
+using Lab04.Tools;
+using Lab04.Tools.DataStorage;
+using Lab04.Tools.Managers;
+using Lab04.Tools.Navigation;
+
+namespace Lab04.ViewModels
+{
+    internal class MainWindowViewModel : BaseViewModel, ILoaderOwner, IContentOwner
+    {
+        #region Fields
+        private Visibility _loaderVisibility = Visibility.Hidden;
+        private bool _isControlEnabled = true;
+        private INavigatable _content;
+        #endregion
+
+        #region Properties
+        public INavigatable Content
+        {
+            get => _content;
+            set
+            {
+                _content = value;
+                OnPropertyChanged();
+            }
+        }
+        public Visibility LoaderVisibility
+        {
+            get => _loaderVisibility;
+            set
+            {
+                _loaderVisibility = value;
+                OnPropertyChanged();
+            }
+        }
+        public bool IsControlEnabled
+        {
+            get => _isControlEnabled;
+            set
+            {
+                _isControlEnabled = value;
+                OnPropertyChanged();
+            }
+        } 
+        #endregion
+
+        internal MainWindowViewModel()
+        {
+            StationManager.Initialize(new SerializedDataStorage());
+            LoaderManager.Instance.Initialize(this);
+            NavigationManager.Instance.Initialize(new ZodiacDeterminantNavigationModel(this));
+            NavigationManager.Instance.Navigate(ViewType.Main);
+        }
+    }
+}
